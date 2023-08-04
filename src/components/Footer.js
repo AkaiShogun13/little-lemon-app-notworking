6061b261508c0f1e658121a98e2951a5e3c6e82d
AkaiShogun13 <127123160+AkaiShogun13@users.noreply.github.com>
2023-08-04 13:29:26 +0100

    new commits

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+export default function Footer() {
+    return (
+        <footer>
+            <img src={"/assets/footer-logo.png"}
+            className="footer-logo"
+            alt="Little lemon logo">
+            </img>
+            <ul>
+                <h3>Doormat Navigation</h3>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">About</a>
+                </li>
+                <li>
+                    <a href="/">Menu</a>
+                </li>
+                <li>
+                    <a href="/">Reservation</a>
+                </li>
+                <li>
+                    <a href="/">Order Online</a>
+                </li>
+                <li>
+                    <a href="/">Login</a>
+                </li>
+            </ul>
+            <ul>
+                <h3>Contact</h3>
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+            <ul>
+                <h3>Connect</h3>
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>Join Us!</li>
+            </ul>
+        </footer>
+    );
+}
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={"/assets/nav-logo.png"}
+            className="nav-logo"
+            alt="Little Lemon logo">
+            </img>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">About</a>
+                </li>
+                <li>
+                    <a href="/">Menu</a>
+                </li>
+                <li>
+                    <a href="/">Reservation</a>
+                </li>
+                <li>
+                    <a href="/">Order Online</a>
+                </li>
+                <li>
+                    <a href="/">Login</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
